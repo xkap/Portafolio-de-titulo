7ae1dccf1581bdb5a15cd36d133b16e46b3a17ee
Lucas Aedo <42651789+xkap@users.noreply.github.com>
2021-05-11 04:30:28 +0200

    Delete Sitio web directory

--- Sitio web/README.md
+++
@@ -1,1 +0,0 @@
-Proyecto final de Carrera. Sitio Web con arquitectura inspirada en microservicios usando principalmente Node JS y APIs REST en Node y Django
--- Sitio web/public/js/payment-confirmed.js
+++
@@ -1,9 +0,0 @@
-// New order javascript
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        window.location.replace('./../'); // redirecting to index
-    }, 10 * 4500);
-});
-
-
-
